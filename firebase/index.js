e08721e9a655123a3b05102cfc5815f1d915e161
George Ramirez <george.ramirez2@gmail.com>
2020-02-23 23:10:39 -0600

    added auth domain back to config

--- firebase/index.js
+++ firebase/index.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 const config = {
 	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
 	databaseURL: process.env.FIREBASE_DATABASE_URL,
 	projectId: process.env.FIREBASE_PROJECT_ID,
 	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
@@ -12,5 +13,3 @@ if (!firebase.apps.length) {
 }
 const auth = firebase.auth();
 export { auth, firebase };
-// removed from the config
-// authDomain: process.env.FIREBASE_AUTH_DOMAIN,
