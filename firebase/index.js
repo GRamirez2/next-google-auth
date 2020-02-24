04ec24d2873bc5d93ded5c95b1d87136bef08985
George Ramirez <george.ramirez2@gmail.com>
2020-02-23 23:07:44 -0600

    removed the auth Domain from the firebase config, getting an error

--- firebase/index.js
+++ firebase/index.js
@@ -2,7 +2,6 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 const config = {
 	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
 	databaseURL: process.env.FIREBASE_DATABASE_URL,
 	projectId: process.env.FIREBASE_PROJECT_ID,
 	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
@@ -13,3 +12,5 @@ if (!firebase.apps.length) {
 }
 const auth = firebase.auth();
 export { auth, firebase };
+// removed from the config
+// authDomain: process.env.FIREBASE_AUTH_DOMAIN,
